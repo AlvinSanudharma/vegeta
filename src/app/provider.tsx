0e267ba5d7a4d8fd6f45cbbb366d87d1e12e0e6b
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-08-26 13:42:23 +0800

    Auth Service

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "@/store";
 import { SessionProvider } from "next-auth/react";
---
+++ src/services/auth.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api",
+  }),
+  tagTypes: ["auth"],
+  endpoints: (builder) => ({
+    register: builder.mutation<any, any>({
+      query: (body) => ({
+        url: "/auth/sign-up",
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation } = authApi;
